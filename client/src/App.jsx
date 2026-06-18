ab85fa36282ab02b3094cd91928ec36bbe74c072
Pallavi Narsinge <pallavinarsinge123@gmail.com>
2026-06-18 22:14:40 +0530

    Fix App.jsx routing error and resolve Vite 500 internal server error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,41 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Components
 import Navbar from "./components/Navbar";
+
+// Pages
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import Wishlist from "./pages/Wishlist";
+import Orders from "./pages/Orders";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+// Admin
+import Dashboard from "./admin/pages/Dashboard";
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />   {/* 👈 ADD THIS */}
+      <Navbar />
 
       <Routes>
+        {/* USER ROUTES */}
         <Route path="/" element={<Home />} />
         <Route path="/product" element={<Product />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/wishlist" element={<Wishlist />} />
-        <Route path="/orders" element={<MyOrders />} />
+        <Route path="/orders" element={<Orders />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
 
+        {/* ADMIN ROUTE */}
         <Route path="/admin/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
+export default App;
