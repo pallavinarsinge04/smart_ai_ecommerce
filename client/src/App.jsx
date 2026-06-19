0d49baf44dca3cbaf619bbb5d21579ff0740a24e
Pallavi Narsinge <pallavinarsinge123@gmail.com>
2026-06-19 12:09:22 +0530

    Hide navbar on public pages and show only for authenticated users

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,62 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 
-// Components
 import Navbar from "./components/Navbar";
 
-// Pages
 import Home from "./pages/Home";
 import Product from "./pages/Product";
 import Cart from "./pages/Cart";
+import Wishlist from "./pages/Wishlist";
+import Orders from "./pages/Orders";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Dashboard from "./admin/pages/Dashboard";
 
-function App() {
+function Layout() {
+  const location = useLocation();
+
+  const hideNavbar =
+    location.pathname === "/" ||
+    location.pathname === "/login" ||
+    location.pathname === "/register";
+
   return (
-    <BrowserRouter>
-      <Navbar />
+    <>
+      {!hideNavbar && <Navbar />}
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/product" element={<Product />} />
-        <Route path="/cart" element={<Cart />} />
+
         <Route path="/login" element={<Login />} />
+
         <Route path="/register" element={<Register />} />
+
+        <Route path="/product" element={<Product />} />
+
+        <Route path="/cart" element={<Cart />} />
+
+        <Route path="/wishlist" element={<Wishlist />} />
+
+        <Route path="/orders" element={<Orders />} />
+
+        <Route
+          path="/admin/dashboard"
+          element={<Dashboard />}
+        />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Layout />
+    </BrowserRouter>
+  );
+}
